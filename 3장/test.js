b6a9779adb09372b6d59cc71000ad2e1f3d3293b
BangGiSeung <45485177+BangGiSeung@users.noreply.github.com>
2020-04-28 11:46:00 +0900

    2020-04-28 commit

--- 3장/test.js
+++ 3장/test.js
@@ -1,18 +1,2 @@
-const a1 = [1,2,3,4];
-const a2 = [1,'two', 3, null];
-const a3 = [
-    "What the hammer? What the chain?",
-    "In what furnace was thy brain?",
-    "What the anvil? What dread grasp",
-    "Dare its deadly terrors clasp?",
-];
-const a4 = [
-    {name : "Ruby", hardness: 9},
-    {name : "Diamond", hardness: 10},
-    {name : "Topaz", hardness : 8},
-];
-const a5 = [
-    [1,2,3],
-    [4,5,6],
-];
-console.log(a1,a2,a3,a4,a5);
+const d = moment.tz([2016,2,17,9,19], `America/Los_Angeles`).toDate();
+console.log(d);
